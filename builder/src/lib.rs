9be7d4234b16e63f71f4947472ef6386a220211d
951mmm <w1w5w9m5m1m@gmail.com>
2025-03-20 16:16:42 +0800

    builder完成

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,9 @@
 use proc_macro::TokenStream;
 use proc_macro2::Ident;
-use quote::{quote, ToTokens};
-use syn::{spanned::Spanned, token::Type};
+use quote::quote;
+use syn::{
+    parse_macro_input, spanned::Spanned, DeriveInput, Field, LitStr, Type,
+};
 // #[derive(std::fmt::Debug)]
 // pub enum BuilderError {
 //     NoneField(std::string::String),
@@ -19,120 +21,275 @@ use syn::{spanned::Spanned, token::Type};
 // }
 
 // impl std::error::Error for BuilderError {}
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse_macro_input!(input as syn::DeriveInput);
+    let ast = parse_macro_input!(input as DeriveInput);
+    match expand(ast) {
+        Ok(token_stream) => token_stream.into(),
+        Err(e) => e.into_compile_error().into(),
+    }
+}
 
+fn expand(ast: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let struct_ident = &ast.ident;
     let struct_ident_literal = ast.ident.to_string();
     let builder_ident_literal = format!("{}Builder", struct_ident_literal);
     let builder_ident = Ident::new(&builder_ident_literal, ast.span());
 
     let mut builder_fields = quote! {};
-    let mut builder_implementation = quote! {};
-    let mut struct_check_none = quote! {};
-    let mut struct_initialize = quote! {};
-    let mut builder_initialize = quote! {};
+    let mut builder_implementations = quote! {};
+    let mut struct_check_nones = quote! {};
+    let mut builder_initializes = quote! {};
+    let mut struct_initializes = quote! {};
 
     match &ast.data {
         syn::Data::Struct(syn::DataStruct { fields, .. }) => {
+            // let field_idents = fields
+            //     .iter()
+            //     .map(|field| field.ident.as_ref().unwrap())
+            //     .collect::<Vec<_>>();
+
+            // let field_tys = fields.iter().map(|field| &field.ty).collect::<Vec<_>>();
+
+            // let field_each_fn_literals = fields
+            //     .iter()
+            //     .map(|field| get_attr_value(field))
+            //     .collect::<Vec<_>>();
+
             for field in fields {
-                let ty = field.ty.to_token_stream();
-                let field_ident = field.ident.as_ref().unwrap();
-                let option_generic_ty = match &field.ty {
-                    syn::Type::Path(syn::TypePath { path, .. }) => {
-                        let segment_last = path.segments.last().unwrap();
-                        if segment_last.ident.eq("Option".into()) {
-                            match &segment_last.arguments {
-                                syn::PathArguments::AngleBracketed(
-                                    syn::AngleBracketedGenericArguments { args, .. },
-                                ) => {
-                                    debug_assert_eq!(args.len(), 1);
-                                    let generic_ty = args.last().unwrap();
-                                    match generic_ty {
-                                        syn::GenericArgument::Type(ty) => Some(ty),
-                                        _ => None,
-                                    }
-                                }
-                                _ => None,
+                let field_ident = get_field_ident(field);
+                let field_ty = &field.ty;
+                let option_inner_ty = get_generic_inner_ty(field, "Option");
+                let each_fn_literal = get_attr_value(field)?;
+                let vec_inner_ty = get_generic_inner_ty(field, "Vec");
+
+                builder_fields.extend(get_builder_field(
+                    field_ident,
+                    field_ty,
+                    option_inner_ty,
+                    &each_fn_literal,
+                ));
+
+                builder_implementations.extend(get_builder_implementation(
+                    field_ident,
+                    field_ty,
+                    option_inner_ty,
+                    &each_fn_literal,
+                    vec_inner_ty,
+                ));
+
+                struct_check_nones.extend(get_struct_check_none(
+                    field_ident,
+                    option_inner_ty,
+                    &each_fn_literal,
+                ));
+
+                builder_initializes.extend(get_builder_initialize(field_ident, &each_fn_literal));
+
+                struct_initializes.extend(get_struct_initialize(
+                    field_ident,
+                    option_inner_ty,
+                    &each_fn_literal,
+                ));
+            }
+            Ok(quote! {
+                pub struct #builder_ident {
+                    #builder_fields
+                }
+
+                impl #builder_ident {
+                    #builder_implementations
+
+                    pub fn build(&mut self) -> std::result::Result<#struct_ident, std::boxed::Box<dyn std::error::Error>> {
+                        #struct_check_nones
+                        std::result::Result::Ok(
+                            #struct_ident {
+                                #struct_initializes
                             }
-                        } else {
-                            None
+                        )
+                    }
+                }
+
+                impl #struct_ident {
+                    pub fn builder() -> #builder_ident {
+                        #builder_ident {
+                            #builder_initializes
+                        }
+                    }
+                }
+
+            })
+        }
+        _ => unimplemented!(),
+    }
+}
+
+fn get_builder_field(
+    field_ident: &Ident,
+    field_ty: &Type,
+    option_inner_ty: Option<&Type>,
+    each_fn_literal: &Option<String>,
+) -> proc_macro2::TokenStream {
+    if each_fn_literal.is_some() {
+        quote! {
+            #field_ident: #field_ty,
+        }
+    } else if let Some(ty) = option_inner_ty {
+        quote! {
+            #field_ident: std::option::Option<#ty>,
+        }
+    } else {
+        quote! {
+            #field_ident: std::option::Option<#field_ty>,
+        }
+    }
+}
+
+fn get_builder_implementation(
+    field_ident: &Ident,
+    field_ty: &Type,
+    option_inner_ty: Option<&Type>,
+    each_fn_literal: &Option<String>,
+    vec_inner_ty: Option<&Type>,
+) -> proc_macro2::TokenStream {
+    let (fn_ident, assign_expr, arg_ty) = match each_fn_literal {
+        Some(fn_literal) => {
+            debug_assert!(vec_inner_ty.is_some());
+            let vec_inner_ty = vec_inner_ty.unwrap();
+            let fn_ident = Ident::new(fn_literal, field_ident.span());
+            let assign_expr = quote! {
+                self.#field_ident.push(#fn_ident)
+            };
+            (fn_ident, assign_expr, vec_inner_ty)
+        }
+        None => (
+            field_ident.clone(),
+            quote! {self.#field_ident = std::option::Option::Some(#field_ident)},
+            field_ty,
+        ),
+    };
+
+    match option_inner_ty {
+        Some(ty) => {
+            quote! {
+                fn #fn_ident(&mut self, #fn_ident: #ty) -> &mut Self {
+                    #assign_expr;
+                    self
+                }
+            }
+        }
+        None => {
+            quote! {
+                fn #fn_ident(&mut self, #fn_ident: #arg_ty) -> &mut Self {
+                    #assign_expr;
+                    self
+                }
+            }
+        }
+    }
+}
+
+fn get_struct_check_none(
+    field_ident: &Ident,
+    option_inner_ty: Option<&Type>,
+    each_fn_literal: &Option<String>,
+) -> proc_macro2::TokenStream {
+    match option_inner_ty {
+        Some(_) => quote! {},
+        None => match each_fn_literal {
+            Some(_) => quote! {},
+            None => quote! {
+                if self.#field_ident.is_none() {
+                    return std::result::Result::Err(format!("field is none: {}", stringify!(#field_ident)).into());
+                }
+            },
+        },
+    }
+}
+
+fn get_builder_initialize(
+    field_ident: &Ident,
+    each_fn_literal: &Option<String>,
+) -> proc_macro2::TokenStream {
+    match each_fn_literal {
+        Some(_) => {
+            quote! {
+                #field_ident: vec![],
+            }
+        }
+        None => {
+            quote! {
+                #field_ident: std::option::Option::None,
+            }
+        }
+    }
+}
+
+fn get_struct_initialize(
+    field_ident: &Ident,
+    option_inner_ty: Option<&Type>,
+    each_fn_literal: &Option<String>,
+) -> proc_macro2::TokenStream {
+    if each_fn_literal.is_some() {
+        quote! {
+            #field_ident: self.#field_ident.clone(),
+        }
+    } else if option_inner_ty.is_some() {
+        quote! {
+            #field_ident: self.#field_ident.take(),
+        }
+    } else {
+        quote! {
+            #field_ident: self.#field_ident.take().unwrap(),
+        }
+    }
+}
+
+fn get_generic_inner_ty<'a>(field: &'a Field, outer: &str) -> Option<&'a Type> {
+    match &field.ty {
+        syn::Type::Path(syn::TypePath { path, .. }) => {
+            let segment_last = path.segments.last().unwrap();
+            if segment_last.ident.eq(outer) {
+                match &segment_last.arguments {
+                    syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                        args,
+                        ..
+                    }) => {
+                        debug_assert_eq!(args.len(), 1);
+                        let generic_ty = args.last().unwrap();
+                        match generic_ty {
+                            syn::GenericArgument::Type(ty) => Some(ty),
+                            _ => None,
                         }
                     }
                     _ => None,
-                };
-
-                builder_initialize.extend(quote! {
-                    #field_ident: std::option::Option::None,
-                });
-                match option_generic_ty {
-                    Some(option_generic_ty) => {
-                        let option_generic_ty = option_generic_ty.to_token_stream();
-                        builder_fields.extend(quote! {
-                            #field_ident: #ty,
-                        });
-                        builder_implementation.extend(quote! {
-                            fn #field_ident(&mut self, #field_ident: #option_generic_ty) -> &mut Self {
-                                self.#field_ident = std::option::Option::Some(#field_ident);
-                                self
-                            }
-                        });
-                        struct_check_none.extend(quote! {});
-                        struct_initialize.extend(quote! {
-                            #field_ident: self.#field_ident.take(),
-                        });
-                    }
-                    None => {
-                        builder_fields.extend(quote! {
-                            #field_ident: std::option::Option<#ty>,
-                        });
-                        builder_implementation.extend(quote! {
-                        fn #field_ident(&mut self, #field_ident: #ty) -> &mut Self {
-                            self.#field_ident = std::option::Option::Some(#field_ident);
-                            self
-                        }
-                        });
-                        struct_check_none.extend(quote! {
-                            if self.#field_ident.is_none() {
-                                return std::result::Result::Err(format!("field is none: {}", stringify!(#field_ident)).into()); 
-                            }
-                        });
-                        struct_initialize.extend(quote! {
-                            #field_ident: self.#field_ident.take().unwrap(),
-                        });
-                    }
                 }
+            } else {
+                None
             }
         }
-        _ => unimplemented!(),
-    };
-    quote! {
-        pub struct #builder_ident {
-            #builder_fields
-        }
-
-        impl #builder_ident {
-            #builder_implementation
-
-            pub fn build(&mut self) -> std::result::Result<#struct_ident, std::boxed::Box<dyn std::error::Error>> {
-                #struct_check_none
-                std::result::Result::Ok(
-                    #struct_ident {
-                        #struct_initialize
-                    }
-                )
-            }
-        }
-
-        impl #struct_ident {
-            pub fn builder() -> #builder_ident {
-                #builder_ident {
-                    #builder_initialize
-                }
-            }
-        }
-
+        _ => None,
     }
-    .into()
+}
+
+fn get_attr_value(field: &Field) -> syn::Result<Option<String>> {
+    let mut each_fn_literal = None;
+    for attr in &field.attrs {
+        if attr.path().is_ident("builder") {
+            attr.parse_nested_meta(|meta| {
+                if meta.path.is_ident("each") {
+                    let value = meta.value()?;
+                    let string_literal = value.parse::<LitStr>()?;
+                    each_fn_literal = Some(string_literal.value());
+                    return Ok(());
+                }
+                Err(meta.error("expected `builder(each = \"...\")`"))
+            })?
+        }
+    }
+    Ok(each_fn_literal)
+}
+
+fn get_field_ident(field: &Field) -> &Ident {
+    field.ident.as_ref().unwrap()
 }
