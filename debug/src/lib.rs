7f043aa59fd8721ac061c177bf56c395ffa89a03
951mmm <w1w5w9m5m1m@gmail.com>
2025-03-21 11:25:54 +0800

    stage: debug 5

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,9 @@
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
 use syn::{
-    parse_macro_input, parse_quote, spanned::Spanned, Data, DataStruct, DeriveInput, Expr, ExprLit,
-    Field, GenericParam, Generics, Ident, ImplGenerics, Lit, Meta, MetaNameValue, Type,
-    TypeGenerics, TypeParam, WhereClause,
+    parse_macro_input, parse_quote, AngleBracketedGenericArguments, Data, DataStruct, DeriveInput,
+    Expr, ExprLit, Field, GenericArgument, GenericParam, Generics, Ident, ImplGenerics, Lit, Meta,
+    MetaNameValue, PathArguments, Type, TypeGenerics, TypePath, WhereClause,
 };
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
@@ -19,25 +19,37 @@ pub fn derive(input: TokenStream) -> TokenStream {
 fn expand(ast: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let struct_ident = &ast.ident;
     let mut field_calls = quote! {};
+    let mut phantom_data_generic_ty = None;
 
     // field stage
     match &ast.data {
         Data::Struct(DataStruct { fields, .. }) => {
             for field in fields {
                 let field_ident = field.ident.as_ref().unwrap();
+                let field_ty = &field.ty;
+                if phantom_data_generic_ty.is_none() {
+                    phantom_data_generic_ty = get_generic_inner_ty(field_ty, "PhantomData");
+                }
+
                 let attr_debug_lit = get_attr_debug_lit(field);
                 field_calls.extend(field_call(field_ident, attr_debug_lit));
             }
         }
         _ => unimplemented!(),
     };
-
     // generic bound stage
     let generics = ast.generics;
-    let generics = add_trait_bounds(generics);
-    let (impl_generics, ty_generics, where_clause) = &generics.split_for_impl();
-    let impl_expr = impl_expr(struct_ident, &impl_generics, &ty_generics, &where_clause);
+    let generics = add_trait_bounds(generics, phantom_data_generic_ty);
 
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let impl_expr = impl_expr(
+        struct_ident,
+        &impl_generics,
+        &ty_generics,
+        where_clause,
+        phantom_data_generic_ty,
+    );
     let result = quote! {
         #impl_expr {
             fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -49,32 +61,6 @@ fn expand(ast: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     };
 
     Ok(result)
-}
-
-fn add_trait_bounds(mut generics: Generics) -> Generics {
-    for param in &mut generics.params {
-        if let GenericParam::Type(ref mut type_param) = *param {
-            type_param.bounds.push(parse_quote!(std::fmt::Debug));
-        }
-    }
-    generics
-}
-
-fn impl_expr(
-    struct_ident: &Ident,
-    impl_generics: &ImplGenerics,
-    ty_generics: &TypeGenerics,
-    where_clause: &Option<&WhereClause>,
-) -> proc_macro2::TokenStream {
-    match where_clause {
-        Some(where_clause) => quote! {
-            impl #impl_generics std::fmt::Debug for #struct_ident #ty_generics
-            #where_clause
-        },
-        None => quote! {
-            impl #impl_generics std::fmt::Debug for #struct_ident #ty_generics
-        },
-    }
 }
 
 fn field_call(field_ident: &Ident, attr_debug_lit: Option<&Lit>) -> proc_macro2::TokenStream {
@@ -100,4 +86,76 @@ fn get_attr_debug_lit(field: &Field) -> Option<&Lit> {
     }
 
     None
+}
+
+fn get_generic_inner_ty<'a>(field_ty: &'a Type, outer: &str) -> Option<&'a Type> {
+    if let Type::Path(TypePath { path, .. }) = field_ty {
+        let path_last = path.segments.last().unwrap();
+        if path_last.ident.eq(outer) {
+            if let PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }) =
+                &path_last.arguments
+            {
+                let generic_ty = args.last().unwrap();
+
+                if let GenericArgument::Type(ty) = generic_ty {
+                    return Some(ty);
+                }
+            }
+        }
+    }
+
+    None
+}
+
+fn add_trait_bounds(mut generics: Generics, phantom_data_generic_ty: Option<&Type>) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(ref mut type_param) = *param {
+            if let Some(ty) = phantom_data_generic_ty {
+                let ty_ident: Ident = parse_quote!(#ty);
+                if type_param.ident.eq(&ty_ident) {
+                    continue;
+                }
+            }
+            type_param.bounds.push(parse_quote!(std::fmt::Debug));
+        }
+    }
+
+    generics
+}
+
+fn impl_expr(
+    struct_ident: &Ident,
+    impl_generics: &ImplGenerics,
+    ty_generics: &TypeGenerics,
+    where_clause: Option<&WhereClause>,
+    phantom_data_generic_ty: Option<&Type>,
+) -> proc_macro2::TokenStream {
+    let where_clause = get_where_clause(where_clause, phantom_data_generic_ty);
+    quote! {
+        impl #impl_generics std::fmt::Debug for #struct_ident #ty_generics
+        #where_clause
+    }
+}
+
+fn get_where_clause(
+    where_clause: Option<&WhereClause>,
+    phantom_data_generic_ty: Option<&Type>,
+) -> proc_macro2::TokenStream {
+    match (where_clause, phantom_data_generic_ty) {
+        (Some(where_clause), Some(ty)) => {
+            quote! {
+                #where_clause
+                PhantomData<#ty>: std::fmt::Debug,
+            }
+        }
+        (_, None) => {
+            quote! {#where_clause}
+        }
+        (_, Some(ty)) => {
+            quote! {
+                where
+                PhantomData<#ty>: std::fmt::Debug,
+            }
+        }
+    }
 }
